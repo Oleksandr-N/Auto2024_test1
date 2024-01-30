b848ff01c761345fa82e3198ec47225d42c4067a
Oleksandr-N <83112773+Oleksandr-N@users.noreply.github.com>
2024-01-30 20:42:32 +0100

    Added new message, removed 2 lines

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello java");
-        System.out.println("123");
+
+        System.out.println("New line1");
     }
 }
